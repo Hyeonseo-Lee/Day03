0c6e7b60a92fe5e2b60deccfbb79eb2de694e28f
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-16 17:40:29 +0900

    5_7

--- day03mission.py
+++ day03mission.py
@@ -1,9 +1,9 @@
-#5.6
+#5.7
 
 name1 = 'duck'
 name2 = 'gourd'
 name3 = 'spitz'
 
-print('%sy Mc%sface'% (name1.capitalize(),name1.capitalize()))
-print('%sy Mc%sface'% (name2.capitalize(),name2.capitalize()))
-print('%sy Mc%sface'% (name3.capitalize(),name3.capitalize()))
+print('{}y Mc{}face'.format(name1.capitalize(), name1.capitalize()))
+print('{}y Mc{}face'.format(name2.capitalize(), name2.capitalize()))
+print('{}y Mc{}face'.format(name3.capitalize(), name3.capitalize()))
