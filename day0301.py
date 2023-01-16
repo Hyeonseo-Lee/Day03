18df92471b1fc2cce6bc2b835585e9c5aa34dc17
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-16 15:35:54 +0900

    day0301code

--- day0301.py
+++ day0301.py
@@ -1,13 +1,28 @@
-# prime number v0.6
+#input 2 numbers
 
-number = int(input("input number : "))
-is_prime = True
+#start = int(input("start number : "))
+#end = int(input("end number : "))
+#print(start, end)
 
-for k in range(2, number):
-    if number % k == 0:
-        is_prime = False
-        break
-if is_prime:
-    print(f'{number} is prime number.')
-else:
-    print(f'{number} is NOT prime number!')
+
+#start_end = int(input("start and end number : ")).split()
+#print(start_end)
+#print(int(start_end[0]), int(start_end[1]))
+
+start = int(input("input start number : "))
+end = int(input("input end number : "))
+
+if end < start:
+    start, end = end, start
+
+for i in range(start, end+1):
+    #is_prime = True
+
+    if i <= 1:
+        continue
+    for k in range(2, i):
+        if i % k == 0:
+    #       is_prime = False
+            break
+    else:
+        print(i, end=' ')
