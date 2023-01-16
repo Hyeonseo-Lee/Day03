46dbded4f05ceb6cc539a4808c7e6a44d5744ddb
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-16 13:39:02 +0900

    day03_exercise

--- day03.py
+++ day03.py
@@ -1,26 +1,38 @@
+#예제 5.1
+song = """When an eel grabs your arm,
+And it causes great harm,
+That's - a moray!"""
+
+#idx = song.rfind('m')
+#print(idx, song[idx].upper())
 
 
-subjects = ' $ python, data structure, database      $$$'
-print(subjects.find('data'), subjects.index('data'))
-print(subjects.find('help')) # -1로 표현
-print(subjects.index('help')) # Error가 뜸
-print(subjects.rfind('data'), subjects.rindex('data'))
-print(subjects.count('a'))
+#song = song.replace('m', 'M')
+#print(song)
+
+
+#idx = song.rfind('m')
+#song = song.replace(song[idx], song[idx].upper())
+#print(song.startswith('m')
+
+
+song_list = song.split()
+print(song_list)
+song_list[14] = song_list[14].title()
+song_string = ' '.join(song_list)
+print(song_string)
 
 
 
-subjects = ' $  python, data structure, databse    '
-print(subjects.strip('$'))
+#5.2
+questions = ["We don't serve strings around here. Are you a string?", "What is said on Father's Day in the forest?", "What makes the sound 'Sis! Boom! Bah!'?" ]
+answers = ["An exploding sheep.", "No, I'm a frayed knot.", "'Pop!' goes the weasel."]
+print(f'Q:{questions[0]}')
+print(f'A:{answers[0]}')
+print(f'Q:{questions[1]}')
+print(f'A:{answers[1]}')
+print(f'Q:{questions[2]}')
+print(f'A:{answers[2]}')
 
-inha = 'a duck goes into a sea'
-print(inha.replace('a ', 'a nice'))
-pokemons_list = ['피카츄', '꼬부기', '이상해', '파이리']
-pokemons_string = '/'.join(pokemons_list)
-print(pokemons_string)
-univ = 'Inha University'
-print(univ.split('i'))
-print(len(univ))
-print(univ[::2])
-print(univ[5:])
-print(univ[5:15])
-print(univ[-10:])
+#5.3
+print("My kitty cat likes %s, \nMy kitty cat likes %s, \nMy kitty cat fell on his %s And now thinks he's a %s." % 'roast beef', 'ham', 'head', 'clam')
