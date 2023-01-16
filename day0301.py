33e5b38a0c03c57a55ce4d7acb148b49c0fb9b00
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-16 14:42:42 +0900

    day03primev06

--- day0301.py
+++ day0301.py
@@ -1,4 +1,4 @@
-# prime number v0.4
+# prime number v0.6
 
 number = int(input("input number : "))
 is_prime = True
@@ -6,10 +6,8 @@ is_prime = True
 for k in range(2, number):
     if number % k == 0:
         is_prime = False
-
+        break
 if is_prime:
     print(f'{number} is prime number.')
 else:
     print(f'{number} is NOT prime number!')
-
-
