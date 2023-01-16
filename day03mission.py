11af52a260f71095f07d158e6b1875163be1642a
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-16 16:49:16 +0900

    5_5

--- day03mission.py
+++ day03mission.py
@@ -1,4 +1,4 @@
-#5.4
+#5.5
 
 letter = '''Dear {salutation} {name},
 Thank you for your letter. We are sorry that our {product}
@@ -7,7 +7,9 @@ be used in a {room}, especially near any {animals}.
 Send us your receipt and {amount} for shipping and handling.
 We will send you another {product} that, in our tests,
 is {percent}% less likely to have {verbed}.
-Thank you for your support.'''
+Thank you for your support.'''.format(salutation='lovely', name='Lee HanSeo', product='fan', verbed='messed', room='living room', animals='dog', amount=2, percent=70)
+
+print(letter)
 
 
 
