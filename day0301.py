0cf4ebda764de235302d8c0a1072a5fa691a8208
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-16 14:08:49 +0900

    day03while

---
+++ day0301.py
@@ -0,0 +1,31 @@
+#while
+
+
+#while True:
+#    dan = int(input('Dan (0 to quit) : '))
+
+#    if dan == 0:
+#        break
+
+#    if 1 < dan < 10:
+    #if dan >= 2 and dan <= 9:
+#        i = 1
+#        while i < 10:
+#            print('{0} * {1} = {2}'.format(dan, i, dan*i))
+#            i = i + 1
+#        break
+#    else:
+#        print('2에서 9사이의 값을 입력 하세요')
+
+
+numbers = [1, 3, 5]
+position = 0
+while position < len(numbers):
+    number = numbers[position]
+    if number % 2 == 0:
+        print('Found even number', number)
+        break
+    position += 1
+else:
+    print('No even number found')
+print(len(numbers))
