787b58dec24b9b39d73b2d51696ac1eb66ff3fe6
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-16 14:38:31 +0900

    day03primevo4

--- day0301.py
+++ day0301.py
@@ -1,36 +1,15 @@
-# prime number v0.1
+# prime number v0.4
 
 number = int(input("input number : "))
-counts = 0
+is_prime = True
 
-k = 1
-#while k <= number:
-#    if number % k == 0:
-#        counts = counts + 1
-#    k = k + 1
-#if counts == 2:
-#    print(f'{number} is prime number!')
-#else:
-#    print(f'{number} is NOT prime number.')
-
-#prime number vo.2
-
-#for k in range(1, number + 1):
-#    if number % k == 0:
-#        counts = counts + 1
-#if counts == 2:
-    print(f'{number} is prime number!')
-#else:
-#    print(f'{number} is NOT prime number.')
-
-
-#prime number vo.3
 for k in range(2, number):
     if number % k == 0:
-        counts = counts + 1
+        is_prime = False
 
-if counts: #0이 아니면
-    print(f'{number} is NOT prime number.')
+if is_prime:
+    print(f'{number} is prime number.')
 else:
-    print(f'{number} is prime number!')
+    print(f'{number} is NOT prime number!')
+
 
