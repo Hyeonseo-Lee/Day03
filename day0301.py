df4a748beb5200f93dddf33282f5c558d39186ba
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-16 14:35:29 +0900

    day03primenumber

--- day0301.py
+++ day0301.py
@@ -1,31 +1,36 @@
-#while
+# prime number v0.1
+
+number = int(input("input number : "))
+counts = 0
+
+k = 1
+#while k <= number:
+#    if number % k == 0:
+#        counts = counts + 1
+#    k = k + 1
+#if counts == 2:
+#    print(f'{number} is prime number!')
+#else:
+#    print(f'{number} is NOT prime number.')
+
+#prime number vo.2
+
+#for k in range(1, number + 1):
+#    if number % k == 0:
+#        counts = counts + 1
+#if counts == 2:
+    print(f'{number} is prime number!')
+#else:
+#    print(f'{number} is NOT prime number.')
 
 
-#while True:
-#    dan = int(input('Dan (0 to quit) : '))
+#prime number vo.3
+for k in range(2, number):
+    if number % k == 0:
+        counts = counts + 1
 
-#    if dan == 0:
-#        break
-
-#    if 1 < dan < 10:
-    #if dan >= 2 and dan <= 9:
-#        for i in range(1, 10, 1):
-#
-#            print('{0} * {1} = {2}'.format(dan, i, dan*i))
-#            i = i + 1
-#        break
-#    else:
-#        print('2에서 9사이의 값을 입력 하세요')
-
-
-numbers = [1, 3, 5]
-position = 0
-while position < len(numbers):
-    number = numbers[position]
-    if number % 2 == 0:
-        print('Found even number', number)
-        break
-    position += 1
+if counts: #0이 아니면
+    print(f'{number} is NOT prime number.')
 else:
-    print('No even number found')
-print(len(numbers))
+    print(f'{number} is prime number!')
+
